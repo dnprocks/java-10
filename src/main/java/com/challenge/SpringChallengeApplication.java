253b83cdda0385c0244c7aa658df2c7d7bc3bb4a
Daniel Pimentel <dnprocks@yahoo.com.br>
2019-11-17 03:00:08 +0000

    merge

--- src/main/java/com/challenge/SpringChallengeApplication.java
+++ src/main/java/com/challenge/SpringChallengeApplication.java
@@ -2,15 +2,8 @@ package com.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 
 @SpringBootApplication
-=======
-import org.springframework.context.annotation.ComponentScan;
-
-@SpringBootApplication
-@ComponentScan(basePackages = {"com.challenge.entity"})
->>>>>>> 0d43a5304b39842ddf42c337a967d951653114c1
 public class SpringChallengeApplication {
 
 	public static void main(String[] args) {
